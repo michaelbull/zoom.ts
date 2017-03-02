d94c8d3edffbfa8ae5926a33fd6e443d905f8e31
Michael Bull <mikebull94@gmail.com>
2017-03-02 14:13:59 +0000

    Add unit tests for addTransitionEndListener

--- test/unit/Transition.spec.ts
+++ test/unit/Transition.spec.ts
@@ -1,4 +1,7 @@
-import { hasTransitions } from '../../lib/Transition';
+import {
+    hasTransitions,
+    addTransitionEndListener
+} from '../../lib/Transition';
 
 describe('hasTransitions', () => {
     it('should compute the style of the element', () => {
@@ -98,5 +101,68 @@ describe('hasTransitions', () => {
         });
 
         expect(hasTransitions(element)).toBe(true);
+    });
+});
+
+describe('addTransitionEndListener', () => {
+    describe('if the element has transitions', () => {
+        let element: any;
+        let listener: EventListener;
+
+        beforeAll(() => {
+            let computedStyle: any = {
+                WebkitTransitionDuration: '100'
+            };
+
+            element = {
+                style: computedStyle,
+                addEventListener: jasmine.createSpy('addEventListener')
+            };
+
+            window.getComputedStyle = jasmine.createSpy('getComputedStyle').and.callFake((elt: any) => {
+                if (elt === element) {
+                    return computedStyle;
+                }
+            });
+
+            listener = jasmine.createSpy('EventListener');
+            addTransitionEndListener(element, listener);
+        });
+
+        it('should listen for the transitionend event', () => {
+            expect(element.addEventListener).toHaveBeenCalledWith('transitionend', listener, undefined);
+        });
+
+        it('should listen for the WebkitTransitionEnd event', () => {
+            expect(element.addEventListener).toHaveBeenCalledWith('WebkitTransitionEnd', listener, undefined);
+        });
+
+        it('should listen for the MozTransitionEnd event', () => {
+            expect(element.addEventListener).toHaveBeenCalledWith('MozTransitionEnd', listener, undefined);
+        });
+
+        it('should listen for the msTransitionEnd event', () => {
+            expect(element.addEventListener).toHaveBeenCalledWith('msTransitionEnd', listener, undefined);
+        });
+
+        it('should listen for the OTransitionEnd event', () => {
+            expect(element.addEventListener).toHaveBeenCalledWith('OTransitionEnd', listener, undefined);
+        });
+    });
+
+    describe('if the element does not have transitions', () => {
+        it('should fire the event immediately', () => {
+            let element: any = {
+                style: {
+                    MozTransitionEnd: '0'
+                }
+            };
+            let listener: any = jasmine.createSpy('EventListener');
+
+            addTransitionEndListener(element, listener);
+
+            let event: Event = listener.calls.mostRecent().args[0];
+            expect(event.type).toBe('transitionend');
+        });
     });
 });
