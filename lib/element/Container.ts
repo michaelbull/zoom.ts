bc23a6b1291283b7ecf35f78ec637b55fa0a289e
Michael Bull <mikebull94@gmail.com>
2017-03-28 12:35:53 +0100

    Extract class name constants for container

--- lib/element/Container.ts
+++ lib/element/Container.ts
@@ -2,12 +2,14 @@ import { createDiv } from '../Document';
 import { hasClass } from './ClassList';
 import { repaint } from './Element';
 
+const CLASS: string = 'zoom__container';
+
 export function createContainer(document: Document): HTMLDivElement {
-    return createDiv(document, 'zoom__container');
+    return createDiv(document, CLASS);
 }
 
 export function isContainer(element: HTMLElement): boolean {
-    return hasClass(element, 'zoom__container');
+    return hasClass(element, CLASS);
 }
 
 export function refreshContainer(container: HTMLElement, callback: Function): void {
