5eea03b8927466a43c01d392642e9f69b937862f
Michael Bull <mikebull94@gmail.com>
2017-03-29 04:18:12 +0100

    Add rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+import cssnano from 'cssnano';
+import postcss from 'postcss';
+import livereload from 'rollup-plugin-livereload';
+import sass from 'rollup-plugin-sass';
+import serve from 'rollup-plugin-serve';
+import typescript from 'rollup-plugin-typescript2';
+import uglify from 'rollup-plugin-uglify';
+
+let copyright = [
+    'zoom.ts v' + require('./package.json').version,
+    'https://www.michael-bull.com/projects/zoom.ts',
+    '',
+    'Copyright (c) 2017 Michael Bull (https://www.michael-bull.com)',
+    '@license ISC'
+];
+
+let postcssPlugins = [
+    cssnano({
+        autoprefixer: {
+            add: true,
+            remove: false
+        }
+    })
+];
+
+let prodEnv = (process.env.NODE_ENV === 'production');
+
+let configuration = {
+    moduleName: 'zoom',
+    entry: 'index.ts',
+    dest: 'dist/zoom.js',
+    format: 'umd',
+    sourceMap: !prodEnv,
+    banner: '/*!' + '\n' + ' * ' + copyright.join('\n * ') + '\n' + ' */' + '\n',
+    plugins: [
+        typescript({ check: prodEnv }),
+        sass({
+            output: 'dist/zoom.css',
+            processor: css => postcss(postcssPlugins)
+                .process(css)
+                .then(result => result.css)
+        })
+    ]
+};
+
+if (prodEnv) {
+    configuration.plugins.push(uglify({
+        output: {
+            comments: (node, comment) => {
+                return comment.line === 1;
+            }
+        }
+    }));
+} else {
+    configuration.plugins.push(serve({ port: 8080 }), livereload());
+}
+
+export default configuration;
--- test/spec-bundle.ts
+++ test/spec-bundle.ts
@@ -1,9 +1,9 @@
-import RequireContext = __WebpackModuleApi.RequireContext;
-
-Error.stackTraceLimit = Infinity;
-
-function requireAll(ctx: RequireContext): void {
-    ctx.keys().forEach(ctx);
-}
-
-requireAll(require.context('./unit', true, /\.spec\.ts$/));
+// import RequireContext = __WebpackModuleApi.RequireContext;
+//
+// Error.stackTraceLimit = Infinity;
+//
+// function requireAll(ctx: RequireContext): void {
+//     ctx.keys().forEach(ctx);
+// }
+//
+// requireAll(require.context('./unit', true, /\.spec\.ts$/));
