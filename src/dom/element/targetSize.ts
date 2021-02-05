ae960c057e5e6c8c61c68f331a6ef9bca1bf917e
Michael Bull <mikebull94@gmail.com>
2021-02-05 23:51:15 +0000

    Simplify targetSize

--- src/dom/element/targetSize.ts
+++ src/dom/element/targetSize.ts
@@ -2,11 +2,8 @@ import { Vector2 } from '../../math';
 import { targetDimension } from './targetDimension';
 
 export function targetSize(element: Element): Vector2 {
-    let targetWidth = targetDimension(element, 'width');
-    let targetHeight = targetDimension(element, 'height');
-
     return {
-        x: targetWidth,
-        y: targetHeight
+        x: targetDimension(element, 'width'),
+        y: targetDimension(element, 'height')
     };
 }
